dbe586e76521d34166f52ad7d13d71e344693428
Arne Boockmeyer <github@arneboockmeyer.de>
2020-01-04 15:10:27 +0000

    Add conditions

---
+++ filter/condition/__init__.py
@@ -0,0 +1,11 @@
+from .Diagnosis import Diagnosis
+from .Procedure import Procedure
+from .Material import Material
+from .Generic import Generic
+
+__all__ = [
+    'Diagnosis',
+    'Procedure',
+    'Material',
+    'Generic'
+]
