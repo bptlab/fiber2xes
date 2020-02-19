fba5db0b3d6542b2cd6f57f58ffd4e792c1cb717
Simon Siegert <sisiegert@uni-potsdam.de>
2020-02-19 15:36:18 +0100

    Clean up and comment example.py

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 import sys
 from datetime import datetime
+
 from fiber import Cohort
 from fiber.condition import (
     Diagnosis,
 )
+
 from fiber2xes import (
     cohort_to_event_log,
     save_event_log_to_file
@@ -11,18 +13,27 @@ from fiber2xes import (
 from fiber2xes.filter.condition import Diagnosis as DiagnosisFilter
 
 # CONFIG START
-cohort_filter = Diagnosis("M54.5", "ICD-10")
-cohort_size = 5
-trace_type = "mrn"
-trace_filter = DiagnosisFilter("M54.5")
+cohort_filter = Diagnosis("M54.5", "ICD-10") # Fiber filter (See fiber documentation)
+cohort_size = 5 # Number of patients in the cohort
+trace_type = "mrn" # Trace type should be either "mrn" or "visit"
+trace_filter = DiagnosisFilter("M54.5") # Filter for traces in XES log
 # CONFIG END
 
-
-dateTimeObj = datetime.now()
-timestampStr = dateTimeObj.strftime("%Y%m%d-%H%M%S")
+# Query cohort of patients from database
 cohort = Cohort(cohort_filter, limit=cohort_size)
 print(f'{len(cohort.mrns())} patients in the cohort')
 
-event_log = cohort_to_event_log(cohort, trace_type, trace_filter=trace_filter, remove_unlisted=True, verbose=False, remove_duplicates=True)
+# Create XES event log with fiber2xes
+event_log = cohort_to_event_log(
+  cohort,
+  trace_type,
+  trace_filter=trace_filter,
+  remove_unlisted=True,
+  verbose=False,
+  remove_duplicates=True,
+)
 
-save_event_log_to_file(event_log, file_path="./xes-logs/log_" + timestampStr + "_" + trace_type + "_" + str(cohort_size) + "-2.xes")
+# Write event log to file
+dateTimeObj = datetime.now()
+timestampStr = dateTimeObj.strftime("%Y%m%d-%H%M%S")
+save_event_log_to_file(event_log, file_path="./xes-logs/log_" + timestampStr + "_" + trace_type + "_" + str(cohort_size) + ".xes")
