16a057f5aeb495c81ed764307c76f0b20d6b7a0a
Arne Boockmeyer <github@arneboockmeyer.de>
2020-02-23 16:34:24 +0100

    Add method documentation of the filters

--- fiber2xes/filter/operator/Not.py
+++ fiber2xes/filter/operator/Not.py
@@ -1,9 +1,25 @@
 class Not(object):
-    def __init__(self, filter1):
-        self.filter1 = filter1
+    """It takes on filter and inverts its result for the given event or trace
+
+    Keyword arguments:
+    filter -- the filter
+    """
+
+    def __init__(self, filter):
+        self.filter = filter
 
     def is_relevant_trace(self, trace):
-        return not self.filter1.is_relevant_trace(trace)
+        """Applies the filter on the trace and returns the inverted result
+
+        Keyword arguments:
+        trace -- the trace
+        """
+        return not self.filter.is_relevant_trace(trace)
 
     def is_relevant_event(self, event):
-        return not self.filter1.is_relevant_event(event)
+        """Applies the filter on the event and returns the inverted result
+
+        Keyword arguments:
+        event -- the event
+        """
+        return not self.filter.is_relevant_event(event)
