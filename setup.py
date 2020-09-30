3056e47fc1d90b2cce4ca09b8b6947e24eeb3c4f
Finn Klessascheck <finn.klessascheck@student.hpi.uni-potsdam.de>
2020-09-30 11:38:17 +0200

    Clarify dev dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, find_packages  # type: ignore
 
+
+requirements = [
+    'sqlalchemy>=1.3.3',
+    'sqlalchemy-hana>=0.3.0',
+    'opyenxes>=0.3.0',
+    'pandas>=0.25.3',
+    'pyspark>=2.4.5'
+]
+
+
 setup(
     name='fiber2xes',
     version='0.9',
@@ -9,12 +19,18 @@ setup(
     author_email='arne.boockmeyer@student.hpi.de, finn.klessascheck@student.hpi.de, francois.peverali@student.hpi.de, martin.meier@student.hpi.de, simon.siegert@student.hpi.de, tom.lichtenstein@student.hpi.de',
     keywords='fiber fiber2xes xes',
     packages=find_packages(),
-    install_requires=[
-        'sqlalchemy>=1.3.3',
-        'sqlalchemy-hana>=0.3.0',
-        'opyenxes>=0.3.0',
-        'pandas>=0.25.3',
-        'pyspark>=2.4.5'
-    ],
+    install_requires=requirements,
+    extras_require={
+        'dev': [
+            'typing-extensions>=3.7.4.3',
+            'mypy>=0.782',
+            'mypy-extensions>=0.4.3',
+            'data-science-types',
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+
+        ]
+    },
     include_package_data=True,
 )
