c4b61a0445fe745aa4db3ecb1669d60acc44d199
Finn Klessascheck <finn.klessascheck@student.hpi.uni-potsdam.de>
2020-11-04 12:27:06 +0100

    Update abstraction tests

--- test/abstraction/test_abstraction_regex.py
+++ test/abstraction/test_abstraction_regex.py
@@ -7,14 +7,14 @@ delimiter = ","
 
 def test_no_abstraction():
     anamnesis = get_abstract_event_name(abstraction_path=None,
-                                        abstraction_exact_match=True,
+                                        abstraction_exact_match=False,
                                         abstraction_delimiter=delimiter,
                                         event_name="some_event",
                                         anamnesis=True)
     assert anamnesis == 'Anamnesis: some_event'
 
     anamnesis_discard = get_abstract_event_name(abstraction_path=None,
-                                                abstraction_exact_match=True,
+                                                abstraction_exact_match=False,
                                                 abstraction_delimiter=delimiter,
                                                 event_name="some_event",
                                                 anamnesis_events="none",
@@ -22,7 +22,7 @@ def test_no_abstraction():
     assert anamnesis_discard == 'Anamnesis: some_event'
 
     no_anamenesis = get_abstract_event_name(abstraction_path=None,
-                                            abstraction_exact_match=True,
+                                            abstraction_exact_match=False,
                                             abstraction_delimiter=delimiter,
                                             event_name="some_event",
                                             anamnesis=False)
@@ -32,115 +32,115 @@ def test_no_abstraction():
 
 def test_group():
     group_anamnesis = get_abstract_event_name(abstraction_path=abstraction_path,
-                                              abstraction_exact_match=True,
+                                              abstraction_exact_match=False,
                                               abstraction_delimiter=delimiter,
-                                              event_name="groupTermA",
+                                              event_name="xyzgroupTermAxyz",
                                               anamnesis=True)
     assert group_anamnesis == 'Anamnesis: groupTermA'
 
     group_anamnesis_discarded = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                        abstraction_exact_match=True,
+                                                        abstraction_exact_match=False,
                                                         abstraction_delimiter=delimiter,
                                                         anamnesis_events="none",
-                                                        event_name="groupTermA",
+                                                        event_name="xyzgroupTermAxyz",
                                                         anamnesis=True)
-    assert group_anamnesis_discarded == 'groupTermA'
+    assert group_anamnesis_discarded == 'groupTermA'  # todo: should be none
 
     group = get_abstract_event_name(abstraction_path=abstraction_path,
-                                    abstraction_exact_match=True,
+                                    abstraction_exact_match=False,
                                     abstraction_delimiter=delimiter,
-                                    event_name="groupTermB",
+                                    event_name="xyzgroupTermBxyz",
                                     anamnesis=False)
     assert group == 'groupTermB'
 
 
 def test_blacklist():
     blacklisted_term = get_abstract_event_name(abstraction_path=abstraction_path,
-                                               abstraction_exact_match=True,
+                                               abstraction_exact_match=False,
                                                abstraction_delimiter=delimiter,
-                                               event_name="blacklistTermA")
+                                               event_name="xyzblacklistTermAxyz")
     assert blacklisted_term == None
 
 
 def test_whitelist():
     whitelisted_term = get_abstract_event_name(abstraction_path=abstraction_path,
-                                               abstraction_exact_match=True,
+                                               abstraction_exact_match=False,
                                                abstraction_delimiter=delimiter,
-                                               event_name="whitelistTermA")
+                                               event_name="xyzwhitelistTermAxyz")
 
     assert whitelisted_term == 'whitelistTermA'
 
     whitelisted_term_anamnesis = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                         abstraction_exact_match=True,
+                                                         abstraction_exact_match=False,
                                                          abstraction_delimiter=delimiter,
                                                          anamnesis=True,
-                                                         event_name="whitelistTermB")
+                                                         event_name="xyzwhitelistTermBxyz")
 
     assert whitelisted_term_anamnesis == 'Anamnesis: whitelistTermB'
 
     whitelisted_term_anamnesis_discarded = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                                   abstraction_exact_match=True,
+                                                                   abstraction_exact_match=False,
                                                                    abstraction_delimiter=delimiter,
                                                                    anamnesis=True,
                                                                    anamnesis_events="none",
-                                                                   event_name="whitelistTermB")
+                                                                   event_name="xyzwhitelistTermBxyz")
 
     assert whitelisted_term_anamnesis_discarded == 'whitelistTermB'
 
 
 def test_abstraction():
     abstracted_term_a = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                abstraction_exact_match=True,
+                                                abstraction_exact_match=False,
                                                 abstraction_delimiter=delimiter,
-                                                event_name="abstractATermA")
+                                                event_name="xyzabstractATermAxyz")
 
     assert abstracted_term_a == "AbstractA"
 
     abstracted_anamnesis_a = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                     abstraction_exact_match=True,
+                                                     abstraction_exact_match=False,
                                                      abstraction_delimiter=delimiter,
                                                      anamnesis=True,
-                                                     event_name="abstractATermB")
+                                                     event_name="xyzabstractATermBxyz")
 
     assert abstracted_anamnesis_a == 'Anamnesis: AbstractA'
 
     abstracted_anamnesis_a_discarded = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                               abstraction_exact_match=True,
+                                                               abstraction_exact_match=False,
                                                                abstraction_delimiter=delimiter,
                                                                anamnesis=True,
                                                                anamnesis_events="none",
-                                                               event_name="abstractATermB")
+                                                               event_name="xyzabstractATermBxyz")
 
-    assert abstracted_anamnesis_a_discarded == 'AbstractA'
+    assert abstracted_anamnesis_a_discarded == 'AbstractA'  # todo: should be none
 
     abstracted_term_b = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                abstraction_exact_match=True,
+                                                abstraction_exact_match=False,
                                                 abstraction_delimiter=delimiter,
-                                                event_name="abstractBTermA")
+                                                event_name="xyzabstractBTermAxyz")
 
     assert abstracted_term_b == "AbstractB"
 
     abstracted_anamnesis_b = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                     abstraction_exact_match=True,
+                                                     abstraction_exact_match=False,
                                                      abstraction_delimiter=delimiter,
                                                      anamnesis=True,
-                                                     event_name="abstractBTermB")
+                                                     event_name="xyzabstractBTermBxyz")
 
     assert abstracted_anamnesis_b == 'Anamnesis: AbstractB'
 
     abstracted_anamnesis_b_discarded = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                               abstraction_exact_match=True,
+                                                               abstraction_exact_match=False,
                                                                abstraction_delimiter=delimiter,
                                                                anamnesis=True,
                                                                anamnesis_events="none",
-                                                               event_name="abstractBTermB")
+                                                               event_name="xyzabstractBTermBxyz")
 
     assert abstracted_anamnesis_b_discarded == 'AbstractB'  # todo: should be none
 
 
 def test_remove_unlisted():
     unlisted_term = get_abstract_event_name(abstraction_path=abstraction_path,
-                                            abstraction_exact_match=True,
+                                            abstraction_exact_match=False,
                                             abstraction_delimiter=delimiter,
                                             anamnesis=False,
                                             remove_unlisted=False,
@@ -149,7 +149,7 @@ def test_remove_unlisted():
     assert unlisted_term == 'unlisted'
 
     unlisted_term_removed = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                    abstraction_exact_match=True,
+                                                    abstraction_exact_match=False,
                                                     abstraction_delimiter=delimiter,
                                                     anamnesis=False,
                                                     remove_unlisted=True,
@@ -158,7 +158,7 @@ def test_remove_unlisted():
     assert unlisted_term_removed == None
 
     unlisted_anamnesis = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                 abstraction_exact_match=True,
+                                                 abstraction_exact_match=False,
                                                  abstraction_delimiter=delimiter,
                                                  anamnesis=True,
                                                  remove_unlisted=False,
@@ -167,7 +167,7 @@ def test_remove_unlisted():
     assert unlisted_anamnesis == 'Anamnesis: unlisted'
 
     unlisted_anamnesis_removed = get_abstract_event_name(abstraction_path=abstraction_path,
-                                                         abstraction_exact_match=True,
+                                                         abstraction_exact_match=False,
                                                          abstraction_delimiter=delimiter,
                                                          anamnesis=True,
                                                          anamnesis_events="none",
